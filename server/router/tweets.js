83ce8f55468c2c981d433475d3e760623f631cf1
Bella <shdudtnr3939@naver.com>
2023-07-22 13:19:39 +0900

    [add] username에 따른 GET 방식 구현

--- server/router/tweets.js
+++ server/router/tweets.js
@@ -1,6 +1,40 @@
 import express from "express";
 import "express-async-errors";
 
+const tweets = [
+  {
+    id: "1",
+    text: "드림코더 화이팅",
+    createdAt: Date.now().toString(),
+    name: "Bella",
+    username: "bella",
+    url: "https://widgetwhats.com/app/uploads/2019/11/free-profile-photo-whatsapp-1.png",
+  },
+  {
+    id: "2",
+    text: "Hi Hi",
+    createdAt: Date.now().toString(),
+    name: "Ellie",
+    username: "ellie",
+  },
+];
+
 const router = express.Router();
 
+// GET /tweets
+// GET /tweets?username=:username
+
+router.get("/", (req, res, next) => {
+  // 기본적으로 /tweets으로 연결되어 있으므로 그냥 /만
+  const username = req.query.username;
+  const data = username
+    ? tweets.filter((t) => t.username === username)
+    : tweets;
+  res.status(200).json(data);
+});
+
+// GET /tweets/:id
+// POST /tweets
+// PUT /tweets/:id
+// DELETE /tweets/:id
 export default router;
